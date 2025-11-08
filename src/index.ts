4bae2e4a50e44fbb613855c2974f010e0bf15fea
beatrizrosa <rosabeatriz438@yahoo.com.br>
2025-11-08 09:40:26 -0300

    fix: Forçando a configuração  CORS

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,14 @@ import cors from 'cors';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const allowedOrigins = [
+  'https://frontend-mini-projeto-dv1k.vercel.app', // <-- A URL do seu frontend (do log de erro)
+  'http://localhost:5173'                         // <-- A URL do seu frontend local
+];
+
 // Middleware para parsear o corpo das requisições como JSON
 app.use(cors({
-  
+  origin: allowedOrigins,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS']
 }));
 app.use(express.json());
